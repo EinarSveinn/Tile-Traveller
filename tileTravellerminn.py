5705301e1425af110aea12e299a901ec558c88d7
Einar Sveinn Pálsson <einarsveinnpalsson@Einars-MacBook-Pro-2.local>
2018-09-18 11:02:59 +0000

    starting

---
+++ tileTravellerminn.py
@@ -0,0 +1,93 @@
+current_tile = 1.1
+
+while current_tile != 3.1:
+    if current_tile == 1.1:
+        print("You can travel: (N)orth.")
+        direction = ''
+        while direction != "N":
+        direction = str(input("Direction: "))
+        if direction == "N" or direction == "n":
+            current_tile = 1.2
+        else:
+            print("Not a valid direction!")
+            continue
+
+    if current_tile == 1.2:
+        print("You can travel: (N)orth or (E)ast or (S)outh.")
+        direction = 'W'
+        direction = str((input("Direction: ")))
+        if direction == "N" or direction == "n":
+            current_tile = 1.3
+        elif direction == "E" or direction == "e":
+            current_tile = 2.2
+        elif direction == "S" or direction == "s":
+            cirrent_tile = 1.1
+        else:
+            print("Not a valid direction!")
+            continue
+
+    if current_tile == 1.3:
+        print("You can travel: (E)ast or (S)outh.")
+        direction = str((input("Direction: ")))
+        if direction == "E" or direction == "e":
+            current_tile = 2.3
+        elif direction == "S" or direction == "s":
+            current_tile = 1.2
+        else:
+            print("Not a valid direction!")
+            continue
+
+    if current_tile == 2.3:
+        print("You can travel: (W)est or (E)ast.")
+        direction = str((input("Direction: ")))
+        if direction == "W" or direction == "w":
+            current_tile = 1.3
+        elif direction == "E" or direction == "e":
+            current_tile = 3.3
+        else:
+            print("Not a valid direction!")
+            continue
+
+    if current_tile == 2.2:
+        print("You can travel: (W)est or (S)outh.")
+        direction = str((input("Direction: ")))
+        if direction == "W" or direction == "w":
+            current_tile = 1.2
+        elif direction == "S" or direction == "s":
+            current_tile = 2.1
+        else:
+            print("Not a valid direction!")
+            continue
+
+    if current_tile == 2.1:
+        print("You can travel: (N)orth.")
+        direction = str((input("Direction: ")))
+        if direction == "N" or direction == "n":
+            current_tile = 2.2
+        else:
+            print("Not a valid direction!")
+            continue
+
+
+    if current_tile == 3.3:
+        print("You can travel: (W)est or (S)outh.")
+        direction = str((input("Direction: ")))
+        if direction == "W" or direction == "w":
+            current_tile = 2.3
+        elif direction == "S" or direction == "s":
+            current_tile = 3.2
+        else:
+            print("Not a valid direction!")
+            continue
+
+    if current_tile == 3.2:
+        print("You can travel: (N)orth or (S)outh.")
+        direction = str((input("Direction: ")))
+        if direction == "N" or direction == "n":
+            current_tile = 3.3
+        elif direction == "S" or direction == "s":
+            current_tile = 3.1
+            print("Victory!")
+        else:
+            print("Not a valid direction!")
+            continue
